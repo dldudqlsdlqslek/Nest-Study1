4eee6c1febccd7528e9d53f9a31b834ff295fda9
dldsudqlsdlqslek <tina4686@gmail.com>
2025-05-27 17:54:56 +0900

    파이프까지

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
-import { Post, Body, Get, Query, Param, Put, Delete } from '@nestjs/common';
-import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
+import { Controller, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
+import { Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto } from './dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interface/cat.interface';
+import { ForbiddenException } from '../exception/forbidden.exception';
+import { HttpExceptionFilter } from 'src/filter/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
@@ -30,5 +32,16 @@ export class CatsController {
     @Delete(':id')
     remove(@Param('id') id: string) {
         return `This action removes a #${id} cat`;
+    }
+
+    @Get('custom_exception')
+    @UseFilters(HttpExceptionFilter)
+    async customExceptionTest(){
+        throw new ForbiddenException();
+    }
+
+    @Get('exception')
+    async exceptionTest() {
+        throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
 }
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/filter/http-exception.filter';
 
 @Module({
   controllers: [CatsController],
-  providers: [CatsService]
+  providers: [CatsService, {
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  }],
+  
 })
 export class CatsModule {}
